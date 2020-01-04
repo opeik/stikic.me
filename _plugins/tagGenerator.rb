920355dd70bee2ab1c0f8b8b9934beb23b22d9aa
Sandro Stikić <sandro@stikic.me>
2020-01-04 21:26:43 +0800

    Update tag page title

--- _plugins/tagGenerator.rb
+++ _plugins/tagGenerator.rb
@@ -9,7 +9,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
-      self.data['title'] = tag
+      self.data['title'] = '#' + tag
     end
   end
 
